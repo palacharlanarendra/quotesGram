94efab9171989684c6431999aa3dc150411d33c2
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-10-09 13:25:56 +0530

    second

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  //DATE IS THE ID
+  id: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  dislikes: {
+    type: Number,
+    required: true,
+  },
+  hearts: {
+    type: Number,
+    required: true,
+  },
+  comments: {
+    type: Array,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Post", postSchema);
